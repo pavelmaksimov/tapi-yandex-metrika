ef020bbbbc061633a617f0c0aaddf92c1185e01b
pmaksimov <vur21@ya.ru>
2019-09-28 13:07:55 +0300

    super commit

--- tapioca_yandex_metrika/tapioca_yandex_metrika.py
+++ tapioca_yandex_metrika/tapioca_yandex_metrika.py
@@ -2,6 +2,7 @@
 import json
 import logging
 import time
+import re
 
 from tapioca import TapiocaAdapter, generate_wrapper_from_adapter, JSONAdapterMixin
 from tapioca.exceptions import ResponseProcessException, ClientError
@@ -10,7 +11,7 @@ from tapioca_yandex_metrika import exceptions
 from .resource_mapping import (
     STATS_RESOURCE_MAPPING,
     LOGSAPI_RESOURCE_MAPPING,
-    MANAGEMENT_RESOURCE_MAPPING
+    MANAGEMENT_RESOURCE_MAPPING,
 )
 
 
@@ -23,7 +24,9 @@ class YandexMetrikaManagementClientAdapter(JSONAdapterMixin, TapiocaAdapter):
     def get_request_kwargs(self, api_params, *args, **kwargs):
         """Обогащение запроса, параметрами"""
         params = super().get_request_kwargs(api_params, *args, **kwargs)
-        params["headers"]["Authorization"] = "OAuth {}".format(api_params["access_token"])
+        params["headers"]["Authorization"] = "OAuth {}".format(
+            api_params["access_token"]
+        )
         return params
 
     def get_error_message(self, data, response=None):
@@ -63,26 +66,131 @@ class YandexMetrikaManagementClientAdapter(JSONAdapterMixin, TapiocaAdapter):
             raise exceptions.YandexMetrikaApiError(response)
         else:
             error_code = int(jdata.get("code", 0))
+            message = jdata.get("message")
 
             if error_code == 429:
                 raise exceptions.YandexMetrikaLimitError(response)
             elif error_code == 403:
                 raise exceptions.YandexMetrikaTokenError(response)
+            elif (
+                message == "Incorrect part number"
+                and api_params.get("receive_all_data", False)
+            ):
+                # Срабатывает при попытке скачать несуществующую часть отчета.
+                # А при получении всех частей отчета автоматически,
+                # всегда идет попытка получить следующий часть.
+                pass
             else:
                 raise exceptions.YandexMetrikaClientError(response)
 
-    def data(self, data, request_kwargs, response, api_params):
-        return data[0] if isinstance(data, list) and data else data
+    def transform_results(self, results, request_kwargs, response, api_params):
+        return results[0] if isinstance(results, list) and results else results
 
 
 class YandexMetrikaLogsapiClientAdapter(YandexMetrikaManagementClientAdapter):
     resource_mapping = LOGSAPI_RESOURCE_MAPPING
 
+    def transform_results(self, results, requests_kwargs, responses, api_params):
+        if (
+            api_params.get("receive_all_data", False)
+            and responses[0].url.find("download") > -1
+        ):
+            # Собирает все части отчета в один текст.
+            data = ""
+            for i in results:
+                cols = i[:i.find("\n")]
+                # Допонлительно удаляется пустая последняя строка.
+                data += i[i.find("\n")+1:-1]
+            return "{}\n{}\n".format(cols, data)
+        else:
+            return results[0] if isinstance(results, list) and results else results
+
+    def to_json(self, data, request_kwargs, response, api_params, *args, **kwargs):
+        """Преобразование данных"""
+        if response.url.find("download") > -1:
+            return [i.split("\t") for i in data.split("\n")][:-1]
+        else:
+            raise NotImplementedError(
+                "Преобразование в JSON доступно только для ответов ресурса download"
+            )
+
+    def retry_request(
+        self,
+        response,
+        tapioca_exception,
+        api_params,
+        count_request_error,
+        *args,
+        **kwargs
+    ):
+        """
+        Условия повторения запроса.
+
+        response = tapioca_exception.client().response
+        status_code = tapioca_exception.client().status_code
+        response_data = tapioca_exception.client().data
+        """
+        status_code = tapioca_exception.client().status_code
+        response_data = tapioca_exception.client().data or {}
+        error_code = int((response_data).get("code", 0))
+        message = response_data.get("message")
+
+        if (
+            message == "Only log of requests in status 'processed' can be downloaded"
+            and api_params.get("wait_report", False)
+            and response.url.find("download") > -1
+        ):
+            logging.info("Включен режим ожидания готовности отчета")
+            logging.info(
+                "Ожидание готовности отчета {} сек.".format(count_request_error * 20)
+            )
+            time.sleep(count_request_error * 20)
+            return True
+
+        return False
+
+    def extra_request(
+        self,
+        api_params,
+        current_request_kwargs,
+        request_kwargs_list,
+        response,
+        current_result,
+    ):
+        if (
+            api_params.get("receive_all_data", False)
+            and response.url.find("download") > -1
+        ):
+            logging.info("Включен режим получения всех данных")
+            logging.info("Будет сделан запрос на получение других частей отчета")
+            url = current_request_kwargs["url"]
+            part = int(re.findall(r"part/([0-9]*)/", url)[0])
+            new_part = part + 1
+            new_url = re.sub(r"part/[0-9]*/", "part/{}/".format(new_part), url)
+            new_request_kwargs = {**current_request_kwargs, "url": new_url}
+            request_kwargs_list.append(new_request_kwargs)
+
+        return request_kwargs_list
+
+    def fill_resource_template_url(self, template, params):
+        if template.find("/part/") > -1 and not params.get("partNumber"):
+            # Принудительно добавляет параметр partNumber, если его нет.
+            params.update(partNumber=0)
+        return template.format(**params)
+
 
 class YandexMetrikaStatsClientAdapter(YandexMetrikaManagementClientAdapter):
     resource_mapping = STATS_RESOURCE_MAPPING
 
-    def retry_request(self, response, tapioca_exception, api_params, count_request_error, *args, **kwargs):
+    def retry_request(
+        self,
+        response,
+        tapioca_exception,
+        api_params,
+        count_request_error,
+        *args,
+        **kwargs
+    ):
         """
         Условия повторения запроса.
 
@@ -98,26 +206,33 @@ class YandexMetrikaStatsClientAdapter(YandexMetrikaManagementClientAdapter):
             logging.error("Превышен лимит запросов")
         elif error_code == 503:
             if count_request_error < api_params.get("retries_if_server_error", 3):
-                logging.warning('Серверная ошибка. Повторный запрос через 3 секунды')
+                logging.warning("Серверная ошибка. Повторный запрос через 3 секунды")
                 time.sleep(3)
                 return True
 
         return False
 
     def extra_request(
-        self, api_params, current_request_kwargs, request_kwargs_list, response, current_result
+        self,
+        api_params,
+        current_request_kwargs,
+        request_kwargs_list,
+        response,
+        current_result,
     ):
-        total_rows = int(current_result['total_rows'])
-        sampled = current_result['sampled']
+        total_rows = int(current_result["total_rows"])
+        sampled = current_result["sampled"]
 
-        logging.info('Наличие семплирования: ' + str(sampled))
+        logging.info("Наличие семплирования: " + str(sampled))
 
         limit = current_result["query"]["limit"]
         offset = current_result["query"]["offset"] + limit
 
         if offset <= total_rows:
             if api_params.get("receive_all_data", False):
-                logging.warning('Получены не все данные. Будет сделан дополнительный запрос')
+                logging.warning(
+                    "Получены не все данные. Будет сделан дополнительный запрос"
+                )
                 new_request_kwargs = current_request_kwargs.copy()
                 new_request_kwargs["params"]["offset"] = offset
                 request_kwargs_list.append(new_request_kwargs)
@@ -125,11 +240,11 @@ class YandexMetrikaStatsClientAdapter(YandexMetrikaManagementClientAdapter):
                 logging.warning("Получены не все данные")
         return request_kwargs_list
 
-    def transform(self, results, request_kwargs, *args, **kwargs):
+    def transform(self, data, request_kwargs, response, api_params, *args, **kwargs):
         """Преобразование данных"""
         new_data = []
-        columns = results[0]["query"]["dimensions"] + results[0]["query"]["metrics"]
-        for result in results:
+        columns = data[0]["query"]["dimensions"] + data[0]["query"]["metrics"]
+        for result in data:
             data = result.pop("data")
             for row in data:
                 dimensions = [i["name"] for i in row["dimensions"]]
@@ -137,8 +252,8 @@ class YandexMetrikaStatsClientAdapter(YandexMetrikaManagementClientAdapter):
                 new_data.append(dimensions + metrics)
         return [columns] + new_data
 
-    def data(self, data, request_kwargs, response, api_params):
-        return data
+    def transform_results(self, results, request_kwargs, response, api_params):
+        return results
 
 
 YandexMetrikaStats = generate_wrapper_from_adapter(YandexMetrikaStatsClientAdapter)
