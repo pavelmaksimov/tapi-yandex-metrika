a8813c557d011e6f58fd66786932e85afa7f0147
pmaksimov <vur21@ya.ru>
2020-10-20 21:29:40 +0300

    тесты для logsapi приведены в порядок

--- tests/losgapi_tests.py
+++ tests/losgapi_tests.py
@@ -1,6 +1,9 @@
 # coding: utf-8
 import logging
+import time
+
 import yaml
+
 from tapi_yandex_metrika import YandexMetrikaLogsapi
 
 logging.basicConfig(level=logging.DEBUG)
@@ -9,62 +12,72 @@ with open("../config.yml", "r") as stream:
     data_loaded = yaml.safe_load(stream)
 
 ACCESS_TOKEN = data_loaded["token"]
+COUNTER_ID = data_loaded["counter_id"]
 
 api = YandexMetrikaLogsapi(
     access_token=ACCESS_TOKEN,
-    default_url_params={'counterId': 178620},
+    default_url_params={"counterId": COUNTER_ID},
     wait_report=True,
-    receive_all_data=False
+    receive_all_data=True,
 )
 
 
 def test_get_allinfo():
     r = api.allinfo().get()
     print(r)
-    print(r().data)
-
-
-def test_create():
-    r = api.create().post(
-        params={
-            "fields": "ym:s:date,ym:s:clientID,ym:s:params,ym:s:startURL,ym:s:endURL,ym:s:dateTime,ym:s:goalsID,ym:s:goalsDateTime,ym:s:lastTrafficSource,ym:s:lastAdvEngine,ym:s:lastReferalSource,ym:s:lastSearchEngineRoot,ym:s:lastSearchEngine,ym:s:lastSocialNetwork,ym:s:lastSocialNetworkProfile,ym:s:referer,ym:s:browserLanguage,ym:s:browserCountry,ym:s:deviceCategory,ym:s:clientTimeZone,ym:s:operatingSystem",
-            "source": "visits",
-            "date1": "2019-09-03",
-            "date2": "2019-09-03"
-        }
-    )
-    print(r)
-
-
-def test_get_info():
-    r = api.info(requestId=4736887).get()
-    print(r)
-
-
-def test_cancel():
-    r = api.cancel(requestId=4736887).post()
-    print(r)
-
-
-def test_clean():
-    r = api.clean(requestId=4728343).post()
-    print(r)
 
 
 def test_evaluate():
     r = api.evaluate().get(
         params={
-            "fields": "ym:s:date,ym:s:clientID",
+            "fields": "ym:s:date",
             "source": "visits",
             "date1": "2019-01-01",
-            "date2": "2019-01-01"
+            "date2": "2019-01-01",
         }
     )
-    print(r().data)
+    print(r)
+
+
+def test_create():
+    r = api.create().post(
+        params={
+            "fields": "ym:s:bounce",
+            "source": "visits",
+            "date1": "2020-04-17",
+            "date2": "2020-04-17",
+        }
+    )
+    global request_id
+    request_id = r().data["log_request"]["request_id"]
+    print(r)
+
+
+def test_get_info():
+    r = api.info(requestId=request_id).get()
+    print(r)
+
+
+def test_cancel():
+    r = api.cancel(requestId=request_id).post()
+    print(r)
+
+
+def test_clean():
+    test_create()
+
+    while True:
+        r_ = api.info(requestId=request_id).get()
+        if r_().data["log_request"]["status"] == "processed":
+            break
+        time.sleep(5)
+
+    r = api.clean(requestId=request_id).post()
+    print(r)
 
 
 def test_download():
-    r = api.download(requestId=5442820, partNumber=2).get()
-    print(r().data[:500])
-    jdata = r().transform()
-    print(jdata[:2])
+    test_create()
+    r = api.download(requestId=request_id).get()
+    data = r().transform()
+    print("\n", len(data), "\n", data[:5])
